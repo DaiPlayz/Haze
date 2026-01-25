ad32dfefb7b44307e2facc32eb193989ab66ec29
scriptisfocus <smokercreator@gmail.com>
2026-01-25 22:24:14 +0100

    Update SwordController.lua

--- libraries/skybridge/SwordController.lua
+++ libraries/skybridge/SwordController.lua
@@ -20,16 +20,21 @@ function SwordController.GetNearestPlayer(range)
     local hrp = character and character:FindFirstChild("HumanoidRootPart")
     if not hrp then return end
 
+    local myTeam = LocalPlayer:GetAttribute("Team")
+
     local closest, closestDist
     for _, plr in ipairs(Players:GetPlayers()) do
         if plr ~= LocalPlayer and plr.Character then
-            local root = plr.Character:FindFirstChild("HumanoidRootPart")
-            local hum = plr.Character:FindFirstChildOfClass("Humanoid")
-            if root and hum and hum.Health > 0 then
-                local dist = (root.Position - hrp.Position).Magnitude
-                if dist <= range and (not closestDist or dist < closestDist) then
-                    closest = plr.Character
-                    closestDist = dist
+            if plr:GetAttribute("Team") ~= myTeam then
+                local root = plr.Character:FindFirstChild("HumanoidRootPart")
+                local hum = plr.Character:FindFirstChildOfClass("Humanoid")
+
+                if root and hum and hum.Health > 0 then
+                    local dist = (root.Position - hrp.Position).Magnitude
+                    if dist <= range and (not closestDist or dist < closestDist) then
+                        closest = plr.Character
+                        closestDist = dist
+                    end
                 end
             end
         end
