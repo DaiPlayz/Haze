042a04ac59e8352afc5a7f1451105f2e58f40ad8
scriptisfocus <smokercreator@gmail.com>
2026-01-27 23:05:05 +0100

    Update Discord.lua

--- libraries/Discord.lua
+++ libraries/Discord.lua
@@ -1,42 +1,18 @@
 local Discord = {}
 
 local HttpService = game:GetService("HttpService")
+
 local Notifications = loadfile("Haze/libraries/Notifications.lua")()
+
 local http_request = (syn and syn.request) or (http and http.request) or request
-local HazeFolder = "Haze"
-local filePath = HazeFolder .. "/Invited.txt"
-
-local function AlreadySent(path)
-    local f = io.open(path, "r")
-    if f then
-        f:close()
-        return true
-    else
-        return false
-    end
-end
-
-local function createFile(path, content)
-    local f = io.open(path, "w")
-    if f then
-        f:write(content or "")
-        f:close()
-    end
-end
 
 local function getInviteCode(url)
     return url:match("discord%.gg/([%w-]+)") or url:match("discord%.com/invite/([%w-]+)")
 end
 
 function Discord:Join(inviteUrl, silent)
-    if AlreadySent(filePath) then
-        if not silent then
-            Notifications:Notify("Success", "Thanks for joining our discord server!", 8)
-        end
-        return false
-    end
-
     local inviteCode = getInviteCode(inviteUrl)
+
     if not inviteCode then
         if not silent then
             Notifications:Notify("Error","Discord invite has expired! Report this to the devs, ScriptIsFocus",3)
@@ -66,13 +42,6 @@ function Discord:Join(inviteUrl, silent)
             })
         })
     end)
-
-    if success then
-        if not AlreadySent(HazeFolder) then
-            os.execute("mkdir " .. HazeFolder)
-        end
-        createFile(filePath, "Joined Haze Discord Invite. https://discord.gg/W92SXVmB5X")
-    end
 
     if not silent then
         if success then
