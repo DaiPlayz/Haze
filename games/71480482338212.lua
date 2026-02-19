abf403b47080f8f0cceaa75d6b3c06cbda8ec529
Spring67#0001 <109059103+Springs67@users.noreply.github.com>
2026-02-19 14:58:52 -0500

    speed fix lawllafewdf[-werg

--- games/71480482338212.lua
+++ games/71480482338212.lua
@@ -57,7 +57,7 @@ RunService.Heartbeat:Connect(function()
         local Humanoid = Character and Character:FindFirstChildOfClass("Humanoid")
         if Humanoid then
             Humanoid.WalkSpeed = SpeedValue
-            Character.PrimaryPart.AssemblyLinearVelocity = Vector3.new(Humanoid.MoveDirection.X * Humanoid.WalkSpeed, Character.PrimaryPart.Velocity.Y, Humanoid.MoveDirection.Z * Humanoid.WalkSpeed)
+            Character.PrimaryPart.AssemblyLinearVelocity = Vector3.new(Humanoid.MoveDirection.X * SpeedValue, Character.PrimaryPart.Velocity.Y, Humanoid.MoveDirection.Z * SpeedValue)
         end
     end
 end)
